5ce68eea96f61fc59c8158844a3da971ce085267
Kevin Erickson <kevin.erick01@gmail.com>
2017-09-05 21:27:40 -0600

    initial

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('styles', () => {
+    return gulp.src('./sass/styles.scss')
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('watch', () => {
+    gulp.watch('./sass/**/*.scss', ['styles']);
+});
+
+gulp.task('default', ['styles']);
---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const logger = require('morgan');
+
+app.set('view engine', 'hbs');
+app.use(logger('combined'))
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+server.listen(8080, () => {
+    console.log('listening on port 8080');
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get("env") === "development") {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render("error", {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render("error", {
+        message: err.message,
+        error: {}
+    });
+});
